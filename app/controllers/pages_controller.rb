50ef20edbaf33436ea21d741c70738d58c419021
Eric Kim <ericskim27@gmail.com>
2016-05-02 01:54:37 +0000

    Created Pages controller. Added index, home, profile, explore pages.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+ # backend code for pages/index
+  def index
+  end
+
+ # backend code for pages/home
+  def home
+  end
+
+ # backend code for pages/profile
+  def profile
+  end
+
+ # backend code for pages/explore
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # Define Routes for pages
+ root 'pages#index'
+
+  get '/home' => 'pages#home'
+
+  get '/profile' => 'pages#profile'
+
+  get '/explore'=> 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
